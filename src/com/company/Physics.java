84579537cf53e023b8d9408a38c8ca80569e4158
JOogway <rut1900@o2.pl>
2015-07-26 02:34:46 +0200

    We got something

--- src/com/company/Physics.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by Maksym on 2015-07-20.
- */
-public class Physics {
-}
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,131 +1,173 @@
 package com.company;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.stage.Stage;
-
+import com.company.Physics;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import static javafx.application.Application.launch;
 
 /**
  * Created by Maksym on 2015-07-20.
  */
-public class Window extends JFrame {
-    XYChart.Series series;
-    JPanel canvas;
-    JLabel jlForce, jlHeight, jlMaxHeight;
-    JTextField jfForce, jfHeight;
-    String MaxHeight = "";
+public class Window extends JFrame implements ActionListener {
+
+
+    public Physics p = new Physics();
+    JButton activator, next1,next2;
+    JLabel jlForce, jlHeight, jlMaxHeight, jlAngle;
+    JTextField jfForce, jfHeight, jfAngle;
+    Color colorToUse;
+    int X1,X2,Y1,Y2,MaxHeight,i,StartHeight = 0,Force,Angle;
 
     public void Window() throws Exception {
-       /* MyChart mc = new MyChart() {
-            @Override
-            protected void layoutChartChildren(double top, double left, double width, double height) {
 
-            }
-        };
-*/
         setSize(1000,700);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
-        //mc.start();
-        ChartInit();
-
+        //ChartInit();
+        Labels();
+        TextFields();
+        p.Physics(getGraphics());
+        Buttons();
+        ExplanationInit();
+        repaint();
 
     }
     public void ChartInit(){
-        Labels();
-        TextFields();
-        Panel();
+        getContentPane().add(activator);
         getContentPane().add(jlForce);
         getContentPane().add(jlHeight);
         getContentPane().add(jlMaxHeight);
+        getContentPane().add(jlAngle);
         getContentPane().add(jfForce);
         getContentPane().add(jfHeight);
-        getContentPane().add(canvas);
+        getContentPane().add(jfAngle);
+        getContentPane().add(p);
+        repaint();
+    }
+    public void ExplanationInit(){
+       getContentPane().add(next1);
+        repaint();
+
+
+
+    }
+    public void EquationsInit(){
+        getContentPane().add(next2);
         repaint();
     }
 
     public void Labels() {
         jlForce = new JLabel("Siła");
-        jlForce.setVisible(true);
         jlForce.setBounds(20, 100, 200, 25);
         jlForce.setFont(new Font("TimesNewRoman", Font.BOLD, 20));
+        jlForce.setToolTipText("Wprowadź Vo");
+        jlForce.setVisible(true);
 
-        jlHeight = new JLabel("Wysokość");
-        jlHeight.setVisible(true);
+
+        jlHeight = new JLabel("Wysokość pocz.");
         jlHeight.setBounds(20, 200, 200, 25);
         jlHeight.setFont(new Font("TimesNewRoman", Font.BOLD, 20));
+        jlHeight.setToolTipText("Domyślnie zero");
+        jlHeight.setVisible(true);
+
+        jlAngle = new JLabel("Kąt");
+        jlAngle.setBounds(20,300,200,25);
+        jlAngle.setFont(new Font("TimesNewRoman", Font.BOLD, 20));
+        jlAngle.setToolTipText("Wprowadź kąt początkowy");
+        jlAngle.setVisible(true);
 
         jlMaxHeight = new JLabel("Max wysokość");
-        jlMaxHeight.setVisible(true);
         jlMaxHeight.setBounds(20, 500, 200, 25);
         jlMaxHeight.setFont(new Font("TimesNewRoman", Font.BOLD, 20));
+        jlMaxHeight.setVisible(true);
 
 
     }
         public void TextFields() {
-            jfForce = new JTextField("");
-            jfForce.setVisible(true);
+            jfForce = new JTextField("0");
             jfForce.setBounds(20, 140, 150, 25);
+            jfForce.setVisible(true);
 
-            jfHeight = new JTextField("");
-            jfHeight.setVisible(true);
+            jfHeight = new JTextField("0");
             jfHeight.setBounds(20, 240, 150, 25);
+            jfHeight.setVisible(true);
+
+            jfAngle = new JTextField("0");
+            jfAngle.setBounds(20,340,150,25);
+            jfAngle.setVisible(true);
         }
-        public void Panel(){
-            canvas = new JPanel();
-            canvas.setVisible(true);
-            canvas.setBounds(250,25,700,600);
-            canvas.setBackground(Color.black);
+
+        public void Buttons(){
+            activator = new JButton("Symuluj");
+            activator.setBounds(20,450,100,50);
+            activator.setVisible(true);
+            activator.addActionListener(this);
+
+            next1 = new JButton("Dalej");
+            next1.setBounds(400,600,100,50);
+            next1.setVisible(true);
+            next1.addActionListener(this);
+
+            next2 = new JButton("Dalej");
+            next2.setBounds(400,600,100,50);
+            next2.setVisible(true);
+            next2.addActionListener(this);
 
         }
-   /* public abstract class MyChart extends Chart {
 
-         public void start(Stage stage) {
-            stage.setTitle("Line Chart Sample");
-            //defining the axes
-            final NumberAxis xAxis = new NumberAxis();
-            final NumberAxis yAxis = new NumberAxis();
-            xAxis.setLabel("Droga");
-            yAxis.setLabel("Wysokość");
-            //creating the chart
-            final LineChart<Number,Number> lineChart =
-                    new LineChart<Number,Number>(xAxis,yAxis);
-
-            lineChart.setTitle("Rzut");
-            //defining a series
-            series = new XYChart.Series();
-            series.setName("Tor");
-            //populating the series with data
-            series.getData().add(new XYChart.Data(1, 23));
-            series.getData().add(new XYChart.Data(2, 14));
-            series.getData().add(new XYChart.Data(3, 15));
-            series.getData().add(new XYChart.Data(4, 24));
-            series.getData().add(new XYChart.Data(5, 34));
-            series.getData().add(new XYChart.Data(6, 36));
-            series.getData().add(new XYChart.Data(7, 22));
-            series.getData().add(new XYChart.Data(8, 45));
-            series.getData().add(new XYChart.Data(9, 43));
-            series.getData().add(new XYChart.Data(10, 17));
-            series.getData().add(new XYChart.Data(11, 29));
-            series.getData().add(new XYChart.Data(12, 25));
-
-            Scene scene  = new Scene(lineChart,800,600);
-            lineChart.getData().add(series);
-            lineChart.setVisible(true);
-            lineChart.setMaxSize(800,600);
-            stage.setScene(scene);
-            stage.show();
-        }
+      /*  public void paint(Graphics g) {
+                g.drawLine(X1, Y1, X2, Y2);
     }*/
-}
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object bSource = e.getSource();
+        if(bSource == activator){
+            Angle = Integer.parseInt(jfAngle.getText());
+            Force = Integer.parseInt(jlForce.getText());
+            StartHeight = Integer.parseInt(jlHeight.getText());
+
+            for(X1=100;X1>0;X1--){
+                X2=X1;
+            for(Y1=100; Y1>0; Y1--) {
+                Y2=Y1;
+                p.getGraphics().drawLine(X1,Y1,X2,Y2);
+            }
+            }
+        }else if(bSource == next1){
+            remove(next1);
+            repaint();
+            EquationsInit();
+        }
+        else if (bSource == next2) {
+            remove(next2);
+            repaint();
+            ChartInit();
+        }
+    }
+
+
+
+}
+ class Physics extends JPanel {
+
+    public void Physics(Graphics g) {
+        FlowLayout fl = new FlowLayout(0,50,40);
+        setLayout(fl);
+        setBounds(220, 50, 750, 600);
+        setBackground(Color.LIGHT_GRAY);
+        setVisible(true);
+        g.setColor(Color.WHITE);
+
+        setAlignmentX(23);
+
+
+    }
+}
 
 
 
